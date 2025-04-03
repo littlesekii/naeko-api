df45437c959bb1821d7f48efcd864829fac0c9b9
littlesekii <contact.sekii@gmail.com>
2025-04-03 19:23:50 -0300

    project initialized

---
+++ src/main/java/com/littlesekii/naeko_api/NaekoApiApplication.java
@@ -0,0 +1,13 @@
+package com.littlesekii.naeko_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaekoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaekoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/littlesekii/naeko_api/NaekoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littlesekii.naeko_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaekoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
